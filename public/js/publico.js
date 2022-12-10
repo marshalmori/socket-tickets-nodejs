96cb4aa7333030e5c3e00f2e64fe358e1fc7bea2
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-12-10 19:50:48 -0300

    Reproducir audio cuando se asigna un ticket

--- public/js/publico.js
+++ public/js/publico.js
@@ -13,17 +13,25 @@ const lblEscritorio4 = document.querySelector("#lblEscritorio4");
 const socket = io();
 
 socket.on("estado-actual", (payload) => {
+  const audio = new Audio("./audio/new-ticket.mp3");
+  audio.play();
+
   const [ticket1, ticket2, ticket3, ticket4] = payload;
 
-  lblTicket1.innerText = "Ticket " + ticket1.numero;
-  lblEscritorio1.innerText = ticket1.escritorio;
-
-  lblTicket2.innerText = "Ticket " + ticket2.numero;
-  lblEscritorio2.innerText = ticket2.escritorio;
-
-  lblTicket3.innerText = "Ticket " + ticket3.numero;
-  lblEscritorio3.innerText = ticket3.escritorio;
-
-  lblTicket4.innerText = "Ticket " + ticket4.numero;
-  lblEscritorio4.innerText = ticket4.escritorio;
+  if (ticket1) {
+    lblTicket1.innerText = "Ticket " + ticket1.numero;
+    lblEscritorio1.innerText = ticket1.escritorio;
+  }
+  if (ticket2) {
+    lblTicket2.innerText = "Ticket " + ticket2.numero;
+    lblEscritorio2.innerText = ticket2.escritorio;
+  }
+  if (ticket3) {
+    lblTicket3.innerText = "Ticket " + ticket3.numero;
+    lblEscritorio3.innerText = ticket3.escritorio;
+  }
+  if (ticket4) {
+    lblTicket4.innerText = "Ticket " + ticket4.numero;
+    lblEscritorio4.innerText = ticket4.escritorio;
+  }
 });
