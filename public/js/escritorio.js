3e4d47522813b3c323e888ad4f2cb9d3a810893b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-12-10 16:49:03 -0300

    Preparar pantalla de escritorio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,33 @@
+// Referências html
+const lblEscritorio = document.querySelector("h1");
+const btnAtender = document.querySelector("button");
 
+const searchParams = new URLSearchParams(window.location.search);
 
-console.log('Escritorio HTML');
+if (!searchParams.has("escritorio")) {
+  window.location = "index.html";
+  throw new Error("A mesa é obrigatório.");
+}
+
+const escritorio = searchParams.get("escritorio");
+lblEscritorio.innerText = escritorio;
+
+const socket = io();
+
+socket.on("connect", () => {
+  btnAtender.disabled = false;
+});
+
+socket.on("disconnect", () => {
+  btnAtender.disabled = true;
+});
+
+socket.on("ultimo-ticket", (ultimo) => {
+  //   lblNuevoTicket.innerHTML = "Ticket " + ultimo;
+});
+
+btnAtender.addEventListener("click", () => {
+  //   socket.emit("siguiente-ticket", null, (ticket) => {
+  //     lblNuevoTicket.innerText = ticket;
+  //   });
+});
